87dac30fc19d096a4dd508f1ce4ba67e72026613
yu-sekita <sekitan.yu@gmail.com>
2019-05-26 15:58:04 +0900

    add article create view and create.html

--- Tech_Blog/blog/views.py
+++ Tech_Blog/blog/views.py
@@ -1,5 +1,7 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
+from blog.forms import ArticleForm
 from blog.models import Article
 
 
@@ -7,3 +9,9 @@ class ArticleListView(generic.ListView):
     model = Article
     context_object_name = 'articles'
     template_name = 'blog/index.html'
+
+
+class ArticleCreateView(generic.CreateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'blog/create.html'
