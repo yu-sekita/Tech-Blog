d17b1e9cdf7ee613bdde97c963a1361d45d21645
yu-sekita <sekitan.yu@gmail.com>
2019-06-02 11:14:33 +0900

    add article detail

--- Tech_Blog/blog/urls.py
+++ Tech_Blog/blog/urls.py
@@ -7,5 +7,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='index'),
-    path('create/', views.ArticleCreateView.as_view(), name='create')
+    path('create/', views.ArticleCreateView.as_view(), name='create'),
+    path('detail/<uuid:pk>/', views.ArticleDetailView.as_view(), name='detail'),
 ]
--- Tech_Blog/blog/views.py
+++ Tech_Blog/blog/views.py
@@ -6,12 +6,21 @@ from blog.models import Article
 
 
 class ArticleListView(generic.ListView):
+    """記事の一覧を表示するview"""
     model = Article
     context_object_name = 'articles'
     template_name = 'blog/index.html'
 
 
 class ArticleCreateView(generic.CreateView):
+    """記事を追加するview"""
     model = Article
     form_class = ArticleForm
     template_name = 'blog/create.html'
+
+
+class ArticleDetailView(generic.DetailView):
+    """記事の詳細を表示するview"""
+    model = Article
+    context_object_name = 'article'
+    template_name = 'blog/detail.html'
