8758a8fd4f5fb7065d7cbc3d15239e5c9f011137
yu-sekita <sekitan.yu@gmail.com>
2019-06-02 12:43:20 +0900

    modify urls checked lint

--- Tech_Blog/blog/urls.py
+++ Tech_Blog/blog/urls.py
@@ -8,5 +8,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='index'),
     path('create/', views.ArticleCreateView.as_view(), name='create'),
-    path('detail/<uuid:pk>/', views.ArticleDetailView.as_view(), name='detail'),
+    path('detail/<uuid:pk>/',
+         views.ArticleDetailView.as_view(),
+         name='detail'),
 ]
