e3655cc762a7c4e055288a80266efa7652300d0c
yu-sekita <sekitan.yu@gmail.com>
2019-06-14 20:04:05 +0900

    add users model

--- Tech_Blog/users/models.py
+++ Tech_Blog/users/models.py
@@ -1,3 +1,96 @@
 from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils import timezone
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """ユーザーマネージャー"""
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """メールアドレスでの登録を必須にする"""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """権限を全てFalseに"""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """スーパーユーザーは全ての権限をTrueに"""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """カスタムユーザーモデル"""
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'
+        )
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        )
+    )
+    date_joined = models.DateTimeField(
+        _('date joined'),
+        default=timezone.now()
+    )
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """ユーザーにメールを送る"""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    def get_full_name(self):
+        """first_nameとlast_nameをスペースで繋げて返す"""
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """ユーザーのshort nameを返す"""
+        return self.first_name
+
+    @property
+    def username(self):
+        """username属性にアクセスした場合にメールアドレスを返す"""
+        return self.email
