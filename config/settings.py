dad04d65829ad94540c4d5f9b8dfec6b1040eb97
yu-sekita <sekitan.yu@gmail.com>
2019-07-10 22:06:25 +0900

    test deploy

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -116,6 +117,8 @@ MARKDOWNX_MARKDOWN_EXTENSIONS = [
 # media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # auth user
 AUTH_USER_MODEL = 'users.User'
