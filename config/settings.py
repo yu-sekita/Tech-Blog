eb3a80a7618f1009f7218c58f05a1999c11b5d38
yu-sekita <sekitan.yu@gmail.com>
2019-07-12 21:39:56 +0900

    deploy test

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -180,4 +179,3 @@ else:
             },
         },
     }
-
