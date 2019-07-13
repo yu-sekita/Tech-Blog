621ee3fea5db03235c219889db3553faea889d0f
yu-sekita <sekitan.yu@gmail.com>
2019-07-13 13:13:15 +0900

    test deploy

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,7 +114,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
@@ -164,7 +162,7 @@ else:
     django_heroku.settings(locals())
     del DATABASES['default']['OPTIONS']['sslmode']
 
-    del STATICFILES_STORAGE
+    # del STATICFILES_STORAGE
 
     ALLOWED_HOSTS.append(env('ALLOWED_HOSTS'))
 
