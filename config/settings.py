f17ccfc9613cb39efb2a3289a08dfd7b204540cf
yu-sekita <sekitan.yu@gmail.com>
2019-07-13 13:58:27 +0900

    add staticfiles_storage

--- config/settings.py
+++ config/settings.py
@@ -164,7 +164,8 @@ else:
     django_heroku.settings(locals())
     del DATABASES['default']['OPTIONS']['sslmode']
 
-    STATICFILES_STORAGE
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # del STATICFILES_STORAGE
 
     ALLOWED_HOSTS.append(env('ALLOWED_HOSTS'))
 
