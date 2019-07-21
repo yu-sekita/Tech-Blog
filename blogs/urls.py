95d19104511ad4d7a329b9146f99fb73471d0c3b
yu-sekita <sekitan.yu@gmail.com>
2019-07-22 06:43:02 +0900

    create Article Edit func

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,8 +7,11 @@ app_name = 'blogs'
 
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='index'),
-    path('create/', views.ArticleCreateView.as_view(), name='create'),
+    path('create/', views.ArticleCreateView.as_view(), name='article_create'),
     path('detail/<uuid:pk>/',
          views.ArticleDetailView.as_view(),
-         name='detail'),
+         name='article_detail'),
+    path('edit/<uuid:pk>/',
+         views.ArticleEditView.as_view(),
+         name='article_edit'),
 ]
