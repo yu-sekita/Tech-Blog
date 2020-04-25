7223beebe8da53a30946f1bade892de0949ae6bf
yu-sekita <sekitan.yu@gmail.com>
2020-04-25 15:21:23 +0700

    disable signup

--- users/urls.py
+++ users/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
     # User create
-    path('user_create/', UserCreateView.as_view(), name='user_create'),
+    # path('user_create/', UserCreateView.as_view(), name='user_create'),
     path('user_create/done/',
          UserCreateDoneView.as_view(), name='user_create_done'),
     path('user_create/complete/<token>/',
