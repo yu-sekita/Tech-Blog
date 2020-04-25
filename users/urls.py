e54ae2ffb6b24e29704589593c731fa2b97737c2
yu-sekita <sekitan.yu@gmail.com>
2020-04-25 15:39:25 +0700

    fix linter

--- users/urls.py
+++ users/urls.py
@@ -10,7 +10,7 @@ from users.views import (
     ProfileView,
     ProfileEditView,
     ProfileImageEditView,
-    UserCreateView,
+    # UserCreateView,
     UserCreateDoneView,
     UserCreateCompleteView,
 )
