803d791d987346ce03c7c2625ff9a51a5031a67c
yu-sekita <sekitan.yu@gmail.com>
2020-11-15 11:35:38 +0700

    adupt linter and create deploy script

--- blogs/models.py
+++ blogs/models.py
@@ -24,7 +24,12 @@ class Article(models.Model):
     title = models.CharField('タイトル', max_length=255)
     # Markdown形式
     text = MarkdownxField('本文')
-    created_at = models.DateTimeField('作成日', auto_now_add=True, null=True, blank=True)
+    created_at = models.DateTimeField(
+        '作成日',
+        auto_now_add=True,
+        null=True,
+        blank=True
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+echo "start deploy"
 
+echo "stop nginx"
+sudo systemctl stop nginx
+
+echo "stop gnicorn"
+pkill gunicorn
+
+echo "start deploy"
+git pull origin master
+
+pipenv run gunicorn config.wsgi --bind 0.0.0.0:8000 -D
+echo "gunicorn started"
+
+sudo systemctl start nginx
+echo "nginx started"
+
+echo "deploy done"
